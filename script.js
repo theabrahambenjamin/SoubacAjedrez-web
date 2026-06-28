809e626197bc0a12b676c7c4262731f6254b295f
theabrahambenjamin <theabrahambenjamin@gmail.com>
2026-06-28 15:52:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,93 @@ function mostrarSeccion(id) {
 
   document.getElementById(id).classList.add("activa");
 }
+let carrito=[];
+
+
+function agregarCarrito(nombre,precio){
+
+
+carrito.push({
+
+nombre:nombre,
+
+precio:precio
+
+});
+
+
+mostrarCarrito();
+
+
+}
+
+
+
+function mostrarCarrito(){
+
+
+let lista=document.getElementById("listaCarrito");
+
+let total=0;
+
+
+lista.innerHTML="";
+
+
+carrito.forEach(producto=>{
+
+
+lista.innerHTML += 
+`
+<p>
+${producto.nombre} - S/ ${producto.precio}
+</p>
+`;
+
+
+total += producto.precio;
+
+
+});
+
+
+document.getElementById("total").innerHTML=total;
+
+
+}
+
+
+
+function comprarWhatsApp(){
+
+
+let mensaje="Hola, quiero comprar:%0A";
+
+
+carrito.forEach(producto=>{
+
+
+mensaje += 
+"- "+producto.nombre+
+" S/"+producto.precio+
+"%0A";
+
+
+});
+
+
+mensaje += 
+"Total: S/"+document.getElementById("total").innerHTML;
+
+
+
+window.open(
+
+"https://api.whatsapp.com/send?phone=51973265025&text="+mensaje,
+
+"_blank"
+
+);
+
+
+}
